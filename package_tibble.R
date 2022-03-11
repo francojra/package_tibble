83a5eb393bef97d674e0602cb926773d01496efd
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-03-11 00:23:17 -0300

    Add files via upload

--- package_tibble.R
+++ package_tibble.R
@@ -52,3 +52,31 @@ mtcars$dis # Selecionando colunas em um data.frame
 
 mtcars_tibble <- as_tibble(mtcars) # Em tibbles isso não funciona
 mtcars_tibble$dis
+
+## Além de nos possibilitar a criar tibbles, o pacote {tibble} possui algumas funções 
+## úteis para manipularmos esses objetos:
+
+## - add_row() e add_column(): para adicionar linhas e colunas a uma tibble;
+
+## - rowid_to_column(): cria uma coluna com um id numérico e sequencial para 
+## as linhas, começando de 1.
+
+mtcars %>% 
+  rowid_to_column() %>% 
+  head(10)
+
+## - has_rownames(): verifica se um data frame possui linhas nomeadas.
+
+## - remove_rownames(): remove o nome das linhas de um data frame.
+
+## - rownames_to_column(): transforma o nome das linhas em uma coluna.
+
+## - column_to_rownames(): transforma uma coluna em nomes das linhas.
+
+rownames_to_column(mtcars, var = "modelo_do_carro") # Nomeia uma coluna com nomes
+remove_rownames(mtcars) # Remove todas as linhas com nomes
+has_rownames(mtcars) # Retorna TRUE ou FALSE
+
+state_dat <- data.frame(state.name, state.area, state.center, state.division)
+column_to_rownames(state_dat, 'state.name') # Transforma uma coluna em nomes
+
